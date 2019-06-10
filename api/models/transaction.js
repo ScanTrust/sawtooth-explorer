d5a4f7e684806df2b22fc9fe0ae390e73b630c14
Bormisov Vlad <bormisov1@gmail.com>
2019-06-10 14:10:48 +0500

    removed accidentally added code

--- api/models/transaction.js
+++ api/models/transaction.js
@@ -5,7 +5,7 @@ let Transaction = new Schema({
     id: String,
     blockId: String,
     batchId: String,
-    payload: String,
+    payload: Buffer,
     signerPublicKey: String,
 });
 
@@ -43,25 +43,6 @@ Transaction._get = function (params, callback) {
       console.log("Err on getting from transactions:", err);
     callback(transactions);
   });
-}
-
-Transaction._getLatestOnAddress = (address, callback) => {
-  Transaction
-    .findOne({address})
-    .sort('-createdAt')
-    .exec(function (err, transaction) {
-      if (err)
-        console.log(err)
-      callback(transaction)
-    })
-}
-
-Transaction._getHistoryOfAddress = (address, callback) => {
-  Transaction.find({address}, null, {sort: {createdAt: -1}}, function (err, transactions) {
-    if (err)
-      console.log(err)
-    callback(transactions)
-  })
 }
 
 module.exports = Transaction;
