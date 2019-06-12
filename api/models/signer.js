b470324475dc9a8a6630d7e4ad9e51da29028f24
Bormisov Vlad <bormisov1@gmail.com>
2019-06-12 14:45:24 +0500

    Signer model

---
+++ api/models/signer.js
@@ -0,0 +1,65 @@
+let mongoose = require('mongoose');
+let Schema = mongoose.Schema;
+
+let Signer = new Schema({
+    publicKey: String,
+    label: String
+});
+
+Signer = mongoose.model('Signer', Signer);
+
+Signer._create = (signer, callback) => {
+    Signer.create(signer, err => {
+        if (err)
+            console.log("Err on creating signer:", err);
+        if (callback)
+            callback()
+    });
+};
+
+Signer._upsert = function (signer, callback) {
+    Signer.findOneAndUpdate({
+        publicKey: signer.publicKey
+    }, signer, { upsert: true }, callback)
+}
+
+function upsertAll(signers, callback) {
+    if (signers.length > 0) {
+        let signer = signers.shift()
+        Signer._upsert(signer, () => upsertAll(signers, callback))
+    } else if (callback) {
+        return callback()
+    }
+}
+
+Signer._upsertAll = upsertAll;
+
+Signer._get = function (params, callback) {
+    Signer.find(params, function (err, signers) {
+        if (err)
+            console.log("Err on getting from signers:", err);
+        callback(signers);
+    });
+}
+
+Signer._getByPublicKey = (publicKey, callback) => {
+    Signer.findOne({publicKey}, (err, signer) => {
+        if (err)
+            console.log('Err on finding signer by publicKey:', err);
+        callback(signer);
+    });
+};
+
+Signer._remove = (signers, callback) => {
+    let query = {
+        '$or': signers.map(signer => ({publicKey: signer.publicKey}))
+    }
+    Signer.remove(query, (err) => {
+        if (err)
+            console.log(err)
+        if (callback)
+            callback()
+    })
+}
+
+module.exports = Signer;
