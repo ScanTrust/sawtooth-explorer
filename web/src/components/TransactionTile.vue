dca7ef5476fbdb77e3e56e693f3d98869070db33
Bormisov Vlad <bormisov1@gmail.com>
2019-06-19 20:33:51 +0500

    changed txn tile display

--- web/src/components/TransactionTile.vue
+++ web/src/components/TransactionTile.vue
@@ -4,7 +4,7 @@
             <img :src="makeAvatar(transaction.id)">
         </v-list-tile-avatar>
         <v-list-tile-content @click="clicked">
-            <v-list-tile-title v-html="transaction.id"></v-list-tile-title>
+            <v-list-tile-title v-html="transaction.payload"></v-list-tile-title>
         </v-list-tile-content>
     </v-list-tile>
 </template>
