84d3c33bf8e94dc05e6db260fd1e8cdb7c7c3036
Vlad Bormisov <bormisov1@gmail.com>
2019-12-19 09:53:17 +0500

    fix default DATABASE_URL

--- api/config.js
+++ api/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     JWT_SECRET: "Very super ultra secret",
-    MONGO_URL: `mongodb://${process.env.DATABASE_URL || 'mongo-db'}:27017/sawtooth-explorer`,
+    MONGO_URL: `mongodb://${process.env.DATABASE_URL || '127.0.0.1'}:27017/sawtooth-explorer`,
     SAWTOOTH_PROXY_PATH: '/sawtooth',
     blockchain: {
         REST_API_URL: process.env.REST_API_URL || 'http://46.173.219.81:8043',
